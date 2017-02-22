1eca17a8ea6610ab5a5fddea9ac9645ba680d3a6
manufg <manufg92@gmail.com>
2017-02-22 12:15:07 +0530

    sign up/in view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,43 @@ Welcome!
 @endsection
 
 @section('content')
-This is the content!
+<div class="row">
+    <div class="col-md-6">
+        <h3>Sign Up</h3>
+        <form action="#" method="POST">
+            <div class="form-group">
+                <label for="email">Your Email</label>
+                <input class="form-control" type="text" name="email" id="email">
+            </div>
+            
+            <div class="form-group">
+                <label for="fname">First Name</label>
+                <input class="form-control" type="text" name="first_name" id="first_name">
+            </div>
+            
+            <div class="form-group">
+                <label for="email">Password</label>
+                <input class="form-control" type="password" name="password" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    
+    <div class="col-md-6">
+        <h3>Sign In</h3>
+        <form action="#" method="POST">
+            <div class="form-group">
+                <label for="email">Your Email</label>
+                <input class="form-control" type="text" name="email" id="email">
+            </div>
+            
+            
+            <div class="form-group">
+                <label for="email">Password</label>
+                <input class="form-control" type="password" name="password" id="password">
+            </div>
+             <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+</div>
 @endsection
