ebd78e0cb0ddc53f90a55b88f964f553d87fdc06
manufg <manufg92@gmail.com>
2017-03-01 12:17:17 +0530

    Middleware and route protection

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,12 +13,14 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 Route::post('/signup',['uses'=>'UserController@postSignUp',
     'as'=>'signup']);
 
 Route::get('/dashboard',['uses'=>'UserController@getDashboard',
-    'as'=>'dashboard']);
+    'as'=>'dashboard',
+    'middleware'=>'auth'
+    ]);
 
 Route::post('/signin',['uses'=>'UserController@postSignIn',
     'as'=>'signin']);
