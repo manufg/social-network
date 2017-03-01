ddeba5331685611e71e919d587749d9b1b28335d
manufg <manufg92@gmail.com>
2017-03-01 14:06:18 +0530

    Dashboard design edited

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,59 @@
-<h1>
-    the dashboard
-</h1>
+@extends('layouts.master')
+@section('title')
+Dashboard
+@endsection
 
 
+@section('content')
+
+
+<section class="row new-post">
+    <div class="col-md-6 col-md-offset-3">
+        <header>
+            <h3>What do you have to say?</h3>
+            <form action="">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post"  rows="5" placeholder="Your Post">
+                   
+                    </textarea>
+                </div>
+                <button class="btn btn-primary"> Create Post</button>
+            </form>
+
+        </header>
+
+    </div>
+</section>
+<section class="row posts">
+    <div class="col-md-6 col-md-offset-3">
+        <header>
+            <h3>What other people say..</h3>
+        </header>
+        <article class="post">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, nisi veritatis quas magnam quo sit alias a accusamus molestiae velit. Quos, distinctio aliquid soluta atque voluptates velit reiciendis numquam dicta.</p>
+            <div class="info">
+                Posted by Manu on 12 Feb 2016 
+            </div>
+            <div class="interaction">
+                <a href="#">Like</a>|
+                <a href="#">Dislike</a>|
+                <a href="#">Edit</a>|
+                <a href="#">Delete</a>
+            </div>
+        </article>
+        
+        <article class="post">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, nisi veritatis quas magnam quo sit alias a accusamus molestiae velit. Quos, distinctio aliquid soluta atque voluptates velit reiciendis numquam dicta.</p>
+            <div class="info">
+                Posted by Manu on 12 Feb 2016 
+            </div>
+            <div class="interaction">
+                <a href="#">Like</a>|
+                <a href="#">Dislike</a>|
+                <a href="#">Edit</a>|
+                <a href="#">Delete</a>
+            </div>
+        </article>
+    </div>
+</section>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
     <head>
         <title>@yield('title')</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+        <link href="{{URL::to('/src/css/main.css')}}" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         @include('includes.header')
