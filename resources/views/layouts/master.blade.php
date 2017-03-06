be4d1e0ca7f118278663315afad3e32e8d490edb
manufg <manufg92@gmail.com>
2017-03-06 18:23:26 +0530

    using jquery to fetch

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,12 +4,20 @@
         <title>@yield('title')</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="{{URL::to('/src/css/main.css')}}" rel="stylesheet" type="text/css"/>
+
+        
+
     </head>
     <body>
         @include('includes.header')
         <div class='container'>
-        @yield('content')
+            @yield('content')
         </div>
+        <!-- Latest compiled and minified JavaScript -->
+              <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+      <script src="{{URL::to('/src/js/app.js')}}" type="text/javascript"></script>
     </body>
 </html>
 
