386ce2cce33ef36f6f0558fc737f33175d06bd97
manufg <manufg92@gmail.com>
2017-03-08 15:17:14 +0530

    Updating view and database

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,9 +1,12 @@
 var postId=0;
+var postBodyElement=null;
 
 $('.post').find('.interaction').find('.edit').on('click',function(event){
     event.preventDefault();
+    postBodyElement=event.target.parentNode.parentNode.childNodes[1];
     var postBody= event.target.parentNode.parentNode.childNodes[1].textContent;
     postId= event.target.parentNode.parentNode.dataset['postid'];
+   
     $('#post-body').val(postBody);
     $('#edit-modal').modal();
 });
@@ -15,7 +18,8 @@ $('#modal-save').on('click',function(){
         data:{body:$('#post-body').val(),postId:postId,_token:token}
     })
             .done(function(msg){
-                console.log(msg['message']);
+                $(postBodyElement).text(msg['new_body']);
+                $('#edit-modal').modal('hide');
             });
     });
 
