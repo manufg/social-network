729515bed34d0f848a88a534dc3a700205c58bc3
manufg <manufg92@gmail.com>
2017-03-10 18:04:35 +0530

    Image upload fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,57 +1,61 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
-Route::post('/signup',['uses'=>'UserController@postSignUp',
-    'as'=>'signup']);
 
-Route::get('/dashboard',['uses'=>'PostController@getDashboard',
-    'as'=>'dashboard',
-    'middleware'=>'auth'
-    ]);
 
-Route::post('/signin',['uses'=>'UserController@postSignIn',
-    'as'=>'signin']);
+Route::post('/signup', ['uses' => 'UserController@postSignUp',
+    'as' => 'signup']);
 
-Route::post('/createpost',['uses'=>'PostController@postCreatePost',
-    'as'=>'post.create',
+Route::get('/dashboard', ['uses' => 'PostController@getDashboard',
+    'as' => 'dashboard',
     'middleware' => 'auth'
 ]);
 
-Route::get('/delete-post/{post_id}',[
-    'uses'=>'PostController@getDeletePost',
-    'as'=>'post.delete',
-     'middleware'=>'auth'
+Route::post('/signin', ['uses' => 'UserController@postSignIn',
+    'as' => 'signin']);
+
+Route::post('/createpost', ['uses' => 'PostController@postCreatePost',
+    'as' => 'post.create',
+    'middleware' => 'auth'
 ]);
 
-Route::get('/account',[
-     'uses'=>'UserController@getAccount',
-    'as'=>'account'
+Route::get('/delete-post/{post_id}', [
+    'uses' => 'PostController@getDeletePost',
+    'as' => 'post.delete',
+    'middleware' => 'auth'
 ]);
 
-Route::get('/updateaccount',[
-     'uses'=>'UserController@postSaveAccount',
-    'as'=>'account.save'
+Route::get('/account', [
+    'uses' => 'UserController@getAccount',
+    'as' => 'account'
 ]);
 
-Route::get('/logout',['uses'=>'UserController@getLogout',
-    'as'=>'logout']);
-
-Route::post('/edit',[
-    'uses'=>'PostController@postEditPost',
-    'as'=>'edit'
+Route::post('/updateaccount', [
+    'uses' => 'UserController@postSaveAccount',
+    'as' => 'account.save'
 ]);
- 
 
+Route::get('/logout', ['uses' => 'UserController@getLogout',
+    'as' => 'logout']);
+
+Route::post('/edit', [
+    'uses' => 'PostController@postEditPost',
+    'as' => 'edit'
+]);
+
+
+Route::get('/userimage/{filename}', ['uses' => 'UserController@getUserImage',
+    'as' => 'account.image']);
